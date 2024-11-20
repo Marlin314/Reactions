e3eb7fe5d35b90ed957dc81477f2e70e6325d501
Marlin314 <meller@uw.edu>
2024-11-20 15:32:32 -0800

    Hello added to sandbox

--- src/sandbox/Hello.java
+++ src/sandbox/Hello.java
@@ -1,7 +1,7 @@
 package sandbox;
 
-public class Test{
+public class Hello{
   public static void main(String[] args){
-    System.out.println("This is as test!");
+    System.out.println("Hello!");
   }
 }
